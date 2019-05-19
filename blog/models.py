4524ba6502ca6959548afe581b354e4a808fb66a
AvvosMeyz <hmztkn@gmail.com>
2019-05-19 17:21:46 +0300

    Initial Commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField('başlık', max_length=200)
+    text = models.TextField('yazı')
+    created_date = models.DateTimeField('yazılan tarih',default=timezone.now)
+    published_date = models.DateTimeField('paylaşılan tarih',blank=True, null=True)
+    tag = models.CharField('etiket', max_length=50)
+    image = models.ImageField(blank=True, null=True)
