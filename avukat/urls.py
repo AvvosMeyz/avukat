ebd764913743c1a196bb1a635efa6856559d1e95
AvvosMeyz <hmztkn@gmail.com>
2019-05-29 04:42:41 +0300

    Add markdown

--- avukat/urls.py
+++ avukat/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,6 +25,7 @@ urlpatterns = [
     path('blog/', include('modules.blog.urls', namespace='home')),
     path('api/', include('modules.api.urls')),
     path('admin/', admin.site.urls),
-]
 
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + staticfiles_urlpatterns()
+
+#urlpatterns += staticfiles_urlpatterns()
--- modules/blog/admin.py
+++ modules/blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import BlogPost
+from django.db import models
+from pagedown.widgets import AdminPagedownWidget
 
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'text', 'tag')
+    list_display = ('title', 'tag', )
+    formfield_overrides = {
+        models.TextField: {'widget': AdminPagedownWidget},
+    }
 
 
 admin.site.register(BlogPost, BlogAdmin)
