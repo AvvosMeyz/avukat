3923ad2f7eeea423e7bc9a8447de4b3362d306a2
AvvosMeyz <hmztkn@gmail.com>
2019-06-26 02:42:46 +0300

    base.html added

--- modules/blog/models.py
+++ modules/blog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.template.defaultfilters import slugify
+
+
+
+kategoriler = [('1', 'Tazminat Hukuku'),
+               ('2', 'Ceza Hukuku'),
+               ('3', 'Medeni Hukuk'),
+               ('4', 'Gayrimenkul Hukuku'),
+               ('5', 'Vergi ve İdare Hukuku'),
+               ('6', 'Bireysel Başvuru'),
+               ('7', 'Arabulucuk'),
+               ('8', 'Hukuk Haberleri'),
+            ]
 
 
 class BlogPost(models.Model):
@@ -8,13 +21,19 @@ class BlogPost(models.Model):
     title = models.CharField('başlık', max_length=200)
     text = models.TextField('yazı')
     created_date = models.DateTimeField('yazılan tarih',default=timezone.now)
-    published_date = models.DateTimeField('paylaşılan tarih',blank=True, null=True)
-    tag = models.CharField('etiket', max_length=50)
+    published_date = models.DateTimeField('paylaşılan tarih', blank=True, null=True)
+    tag = models.CharField('kategori', choices=kategoriler, max_length=50)
+    slug = models.SlugField(max_length=150, default="gezegen_gezegen", unique=True)
     image = models.ImageField(
         blank=True,
         null=True,
         default="static/img/default_blog.jpg", upload_to="static/img/%Y/%m/%d"
     )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(BlogPost, self).save(*args, **kwargs)
+
 
     @property
     def image_url(self):
--- modules/blog/views.py
+++ modules/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import BlogPost
 from django.http import Http404
 from django.db.models import Count
-
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_blog = BlogPost.objects.all()
@@ -18,7 +18,8 @@ def index(request):
 
 def detail(request, id):
     try:
-        blogpost = BlogPost.objects.get(pk=id)
+        blogpost = get_object_or_404(BlogPost, id=id)
+    #    blogpost = BlogPost.objects.get(slug=slug)
         distinct_tags = BlogPost.objects.all().distinct('tag').order_by('tag')
         #Transaction.objects.all().values('actor').annotate(total=Count('actor')).order_by('total')
         distinct_tag_number = BlogPost.objects.all().values('tag').annotate(Count('id')).order_by('tag')
