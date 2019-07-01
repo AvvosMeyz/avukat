61fb0adfbda0c21821ac53038233aa11cc73f8d7
AvvosMeyz <hmztkn@gmail.com>
2019-07-01 16:46:03 +0300

    Deneme

--- avukat/settings.py
+++ avukat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # /home/avvos/dev/samet/avukat
@@ -20,6 +21,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'), # /home/avvos/dev/samet/avukat/static
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -27,9 +30,9 @@ STATICFILES_DIRS = (
 SECRET_KEY = 'ur43!s8i8^td&r4m=b=n=d^uzn*mracx5ws=1=)(t^xh9np)-j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [smt123.herokuapp.com]
 
 
 # Application definition
@@ -50,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,6 +132,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
